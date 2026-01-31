eedef63d5aaa2c7cecf5dc3b9a86f3e91bbed960
Dayna-Marie Pemberton <daynamariejpemberton@gmail.com>
2026-01-31 03:26:07 -0500

    Add the background photos

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="bg-[url('./Images/goat-mobile.png')] bg-cover h-screen lg:bg-[url('./Images/goats.png')] bg-center">
     </div>
   );
 }
