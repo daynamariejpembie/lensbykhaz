20a0bb9a30872e09db6180d43a2c595bd14afbb5
Dayna-Marie Pemberton <daynamariejpemberton@gmail.com>
2026-01-31 01:30:43 -0500

    Save as is

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div>
-      
+    <div >
+    <nav>
+      <ul>
+        <li>Portfolio</li>
+        <li>Session Info</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
     </div>
   );
 }
