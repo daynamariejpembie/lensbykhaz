d86ce3129f42c4c86a2f72dcf154cd2375bf6803
Dayna-Marie Pemberton <daynamariejpemberton@gmail.com>
2026-02-05 22:25:26 -0500

    Create Portfolio Page

--- src/app/Components/Navigation/Navigation.js
+++ src/app/Components/Navigation/Navigation.js
@@ -6,19 +6,19 @@ export default function Navigation() {
             <nav className="w-full flex sm:justify-end">
                 <ul className="flex gap-5 text-stone-50 text-md md:text-xl text-shadow-md/60 md:text-shadow-lg/30 font-serif sm:p-5">
                   <Link
-                  href="./Portfolio"
+                  href="./Components/Navigation/Portfolio"
                   className="hover:text-shadow-lg/80"
                   ><li>Portfolio</li></Link>
                   <Link
-                  href="./Sessions"
-                  className="hover:text-shadow-lg/80"
+                  href="./Components/Navigation/Sessions"
+                  className=" hidden hover:text-shadow-lg/80"
                   ><li>Sessions</li></Link>
                   <Link
-                  href="./About"
+                  href="./Components/Navigation/About"
                   className="hover:text-shadow-lg/80"
                   ><li>About</li></Link>  
                   <Link
-                  href="./Contact"
+                  href="./Components/Navigation/Contact"
                   className="hover:text-shadow-lg/80"
                   ><li>Contact</li></Link>  
                 </ul>
